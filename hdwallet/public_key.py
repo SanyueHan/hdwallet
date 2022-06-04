280f55956cc75876bc7fb08604075a4d5f147ccb
SanyueHan <hansanyue@hotmail.com>
2022-06-04 18:08:00 +0800

    add script code

--- hdwallet/public_key.py
+++ hdwallet/public_key.py
@@ -1,19 +1,29 @@
-from bitsv.format import public_key_to_address
+from bitsv.format import public_key_to_address, address_to_public_key_hash
+from bitsv.transaction import OP_DUP, OP_HASH160, OP_PUSH_20, OP_EQUALVERIFY, OP_CHECKSIG
 from coincurve import PublicKey
 
 from hdwallet.network import NETWORK_API
 
 
 class PubKey:
-    def __init__(self, public_key: bytes):
-        self._public_key = PublicKey(public_key)
-        self._address = public_key_to_address(public_key)
+    def __init__(self, pub_key_bytes: bytes):
+        self._public_key: PublicKey = PublicKey(pub_key_bytes)
+        self._address: str = public_key_to_address(pub_key_bytes)
+        self._scriptcode: bytes = (OP_DUP + OP_HASH160 + OP_PUSH_20 + address_to_public_key_hash(self.address) + OP_EQUALVERIFY + OP_CHECKSIG)
         self._unspents = []
         self._transactions = []
 
     @property
+    def public_key(self):
+        return self.public_key
+
+    @property
     def address(self):
         return self._address
+
+    @property
+    def scriptcode(self):
+        return self._scriptcode
 
     @property
     def balance(self):
