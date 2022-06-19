52812d553ded54c9fe5d6bbb0f2fbcbaa624e647
SanyueHan <hansanyue@hotmail.com>
2022-06-19 18:48:27 +0800

    add some references

---
+++ hdwallet/references.py
@@ -0,0 +1,6 @@
+
+
+class References:
+    XPUB = "https://river.com/learn/terms/x/xpub-extended-public-key/"
+    XPRV = "https://river.com/learn/terms/x/xprv-extended-private-key/"
+    SEED = "https://river.com/learn/terms/s/seed-bitcoin/"
